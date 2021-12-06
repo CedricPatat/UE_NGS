98b05d2c23fb2a1e8da21da3e2f9191e3941461b
CedricPatat <cedric.patat@outlook.fr>
2021-12-06 10:21:05 +0100

    Update pipeline.sh

--- Exome_Sequencing_Pipeline/pipeline.sh
+++ Exome_Sequencing_Pipeline/pipeline.sh
@@ -2,7 +2,7 @@
 #######           PIPELINE SCRIPT             #######
 #####################################################
 
-# Downloading datas 
+# Downloading data
 echo -e '\033[1;0;33m DOWNLOADING DATA \033[0m'
 if [ ! -e patient7.tar.gz ];then     
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe" -O patient7.tar.gz && rm -rf /tmp/cookies.txt  # downloading RNA-Seq data
@@ -12,3 +12,5 @@ if [ ! -e patient7.tar.gz ];then
 else
     echo "RNA-Seq data is already downloaded."
 fi
+
+# Trimming data
