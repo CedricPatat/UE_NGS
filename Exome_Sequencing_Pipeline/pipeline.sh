87f3e2c913d5d3522a867ab65a320b7a6c6116ba
CedricPatat <cedric.patat@outlook.fr>
2022-01-03 21:10:39 +0100

    Update pipeline

--- Exome_Sequencing_Pipeline/pipeline.sh
+++ Exome_Sequencing_Pipeline/pipeline.sh
@@ -8,43 +8,44 @@ echo -e '\033[1;0;33m DOWNLOADING DATA \033[0m'
 
 # Downloading patient data
 if [ ! -e initial_datas/patient7.tar.gz ];then     
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe" -O patient7.tar.gz && rm -rf /tmp/cookies.txt  # downloading RNA-Seq data
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe" -O patient7.tar.gz && rm -rf /tmp/cookies.txt  # downloading data
     tar -zxvf patient7.tar.gz    # data decompression
-    gunzip patient7.exome/*  # .fastq decompression
-    mv *.fastq initial_datas/
-    mv *.tar.gz initial_datas/
+    gunzip patient7.exome/*      # .fastq decompression
+    # mv *.fastq initial_datas/    # moving .fastq files to initial_datas
+    # mv *.tar.gz initial_datas/   # moving compressed file to initial_datas
 else
     echo "RNA-Seq data is already downloaded."
 fi
 
 # Downloading the reference genome
 if [ ! -e index/chr16.fa ];then 
-    wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr16.fa.gz
-    gunzip chr16.fa.gz
-    mv chr16.fa index/chr16.fa
+    wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr16.fa.gz  # download the reference chromosome
+    gunzip chr16.fa.gz             # reference chromosome file decompression
+    mv chr16.fa index/chr16.fa     # moving the reference chromosome file to index
 else
     echo "Reference genome is already downloaded."
 fi
 
 # Downloading the genome annotation
 if [ ! -e gencode.v24lift37.basic.annotation.gtf ];then 
-    wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_24/GRCh37_mapping/gencode.v24lift37.basic.annotation.gtf.gz
-    gunzip gencode.v24lift37.basic.annotation.gtf.gz
+    wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_24/GRCh37_mapping/gencode.v24lift37.basic.annotation.gtf.gz   # download the annotation
+    gunzip *.gtf.gz            # annotation file decompression
+    mv *.gtf initial_datas/*   # moving the annotation file to index
 else
     echo "Annotation is already downloaded."
 fi
 
+echo -e '\033[1;0;33m INDEXING THE REFERENCE GENOME \033[0m'
 # Indexing of the reference genome
 if [ ! -e index/chr16.fa.amb ] || [ ! -e index/chr16.fa.ann ] || [ ! -e index/chr16.fa.bwt ] || [ ! -e index/chr16.fa.fai ] || [ ! -e index/chr16.fa.pac ] || [ ! -e index/chr16.fa.sa ];then 
-    bwa index -a bwtsw index/chr16.fa
+    bwa index -a bwtsw index/chr16.fa   # indexing of the reference chromosome with bwa index
 else
     echo " Indexing of the reference genome has already been performed."
 fi
 
-
-# Trimming data
-echo -e '\033[1;0;33m FILES ANALYSIS WITH TRIMMOMATIC \033[0m'
-chmod +x patient7.exome/*
+# Trimming data with trimmomatic
+echo -e '\033[1;0;33m DATA TRIMMING WITH TRIMMOMATIC  \033[0m'
+chmod +x patient7.exome/*  # gives permissions to files in the directory
 # Retrieving files for analysis
 files=()  # list containing the names of the files
 j=0     # increment index
@@ -59,31 +60,30 @@ h=0
 for i in `seq 0 $(($j-1))`  # Trimmomatic analysis loop on each file
 do
 name=` echo ${files[h]}|cut -d"_" -f1 `
-trimmomatic PE patient7.exome/${files[h]} patient7.exome/${files[h+1]} -baseout $name.fastq LEADING:20 TRAILING:20 MINLEN:50
+trimmomatic PE patient7.exome/${files[h]} patient7.exome/${files[h+1]} -baseout trimmomatic_results/$name.fastq LEADING:20 TRAILING:20 MINLEN:50
 h=$(($h + 2))
 done
 
-# Creating BWA Index
-
-
 
 # Mapping with BWA
+echo -e '\033[1;0;33m MAPPING WITH BWA  \033[0m'
 files=()  # list containing the names of the files
 j=0     # increment index
-for i in `find *_1P.fastq`    # Loop saving each 1P.fastq file name in the list
+for i in `find trimmomatic_results/*_1P.fastq`    # Loop saving each 1P.fastq file name in the list
 do
-name=` echo $i|cut -d"_" -f1 ` 
+name=` echo $i|cut -d"/" -f2 ` 
+name=` echo $name|cut -d"_" -f1 `
 files[j]=$name
 j=$(($j + 1))
 done
 
 for i in `seq 0 $(($j-1))`  # Loop on each file
 do
-bwa mem -M -t 2 -A 2 -E 1 index/chr16.fa ${files[$i]}_1P.fastq ${files[$i]}_2P.fastq > outputs/${files[$i]}
+bwa mem -M -t 2 -A 2 -E 1 index/chr16.fa trimming_results/${files[$i]}_1P.fastq trimming_results/${files[$i]}_2P.fastq > outputs/${files[$i]}
 done
 
 # Processing SAM files
-
+echo -e '\033[1;0;33m PROCESSING SAM FILES WITH SAMTOOLS \033[0m'
 
 files=()  # list containing the names of the files
 j=0     # increment index
@@ -115,7 +115,7 @@ done
 
 
 # Calling somatic variants with Varscan
-
+echo -e '\033[1;0;33m CALLING SOMATIC VARIANTS WITH VARSCAN  \033[0m'
 
 path_to_normal_mpileup=`find outputs/*-N-*.mpileup`
 path_to_tumor_mpileup=`find outputs/*-T-*.mpileup`
@@ -126,6 +126,7 @@ varscan somatic path_to_normal_mpileup path_to_tumor_mpileup varscan_results/$ou
 
 
 # Basic VCF Annotation
+echo -e '\033[1;0;33m BASIC VCF ANNOTATION  \033[0m'
 
 for i in `find varscan_results/*`
 do
