4e365a89bf2afc74a3c90953dd983e2ba34f8155
CedricPatat <cedric.patat@outlook.fr>
2022-01-03 12:29:40 +0100

    Update pipeline

--- Exome_Sequencing_Pipeline/pipeline.sh
+++ Exome_Sequencing_Pipeline/pipeline.sh
@@ -7,7 +7,7 @@
 echo -e '\033[1;0;33m DOWNLOADING DATA \033[0m'
 
 # Downloading patient data
-if [ ! -e patient7.tar.gz ];then     
+if [ ! -e initial_datas/patient7.tar.gz ];then     
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe" -O patient7.tar.gz && rm -rf /tmp/cookies.txt  # downloading RNA-Seq data
     tar -zxvf patient7.tar.gz    # data decompression
     gunzip patient7.exome/*  # .fastq decompression
